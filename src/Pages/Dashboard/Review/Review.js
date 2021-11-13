89c298ec4c9ad151e99524cbc035fb2598ca4669
MK-Chakraborty <mkchakraborty5@gmail.com>
2021-11-13 16:54:52 +0600

    small chenges

--- src/Pages/Dashboard/Review/Review.js
+++ src/Pages/Dashboard/Review/Review.js
@@ -29,10 +29,10 @@ const Review = () => {
         <Container>
             <h3 className="fw-light py-4">Please Leave Your Valuable Feedback.</h3>
             <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column">
-                <input {...register("name", { maxLength: 100 })} value={user.displayName} placeholder="Full Name" className="border-0 border-bottom border-3 fs-6 fw-bold py-3" />
+                <input {...register("name")} value={user.displayName} placeholder="Full Name" className="border-0 border-bottom border-3 fs-6 fw-bold py-3" />
                 <input type="email" {...register("email")} value={user.email} placeholder="Email" className="border-0 border-bottom border-3 fs-6 fw-bold py-3" />
-                <input {...register("msg", { maxLength: 600 })} placeholder="Feedback" className="border-0 border-bottom border-3 fs-6 fw-bold py-3" />
-                <input type="number" {...register("star", { maxLength: 1 })} placeholder="Rate 1 2 3 4 5" className="border-0 border-bottom border-3 fs-6 fw-bold py-3" />
+                <input {...register("msg", {required: true, maxLength: 2000 })} placeholder="Feedback" className="border-0 border-bottom border-3 fs-6 fw-bold py-3" />
+                <input type="number" {...register("star", {required: true, maxLength: 1 })} placeholder="Rate 1 2 3 4 5" className="border-0 border-bottom border-3 fs-6 fw-bold py-3" />
                 <input type="text" {...register("date")} value={today} placeholder="Date" className="border-0 border-bottom border-3 fs-6 fw-bold py-3" />
                 <input type="submit" value="Submit" className="bg-dark bg-gradient text-white fs-5 fw-bold rounded mt-3 py-3 w-50 mx-auto" />
             </form>
